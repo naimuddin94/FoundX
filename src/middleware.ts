f86ded216dd727125c3db5348f4ec7a0a49d6839
naimuddin94 <developernaimuddin@gmail.com>
2024-09-16 16:24:58 +0600

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,19 @@ import { NextResponse } from "next/server";
 
 const AuthRoutes = ["/login", "/register"];
 
-const user = null;
+type IRole = keyof typeof roleBasedRoutes;
+
+const roleBasedRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
+
+const user = {
+  name: "admin",
+  token: "token",
+  role: "USER",
+};
+
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -17,10 +29,18 @@ export function middleware(request: NextRequest) {
     }
   }
 
+  if (user?.role && roleBasedRoutes[user?.role as IRole]) {
+    const routes = roleBasedRoutes[user?.role as IRole];
+
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+
   return NextResponse.redirect(new URL("/login", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/login", "/register"],
+  matcher: ["/profile", "/admin", "/login", "/register"],
 };
