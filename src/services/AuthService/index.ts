c3c73d82e023fb26e6903309e560bab7e911041f
naimuddin94 <developernaimuddin@gmail.com>
2024-09-20 20:43:30 +0600

    login server functionality

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,24 +1,37 @@
-"use server"; 
+"use server";
 
-import axiosInstance from "@/src/lib/AxiosInstance"; // Importing custom Axios instance for making API requests
-import { cookies } from "next/headers"; // Importing cookies from Next.js to set HTTP cookies on the server
-import { FieldValues } from "react-hook-form"; // Importing FieldValues type from react-hook-form to type userData
+import axiosInstance from "@/src/lib/AxiosInstance";
+import { cookies } from "next/headers";
+import { FieldValues } from "react-hook-form";
 
 // Function to register a new user with provided user data
 export const registerUser = async (userData: FieldValues) => {
   try {
-    // Make a POST request to the "/auth/register" endpoint with user data
     const { data } = await axiosInstance.post("/auth/register", userData);
 
-    // If registration is successful, set access and refresh tokens as cookies
     if (data.success) {
-      cookies().set("accessToken", data?.data?.accessToken); // Set access token
-      cookies().set("refreshToken", data?.data?.refreshToken); // Set refresh token
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
     }
 
-    return data; // Return response data
+    return data;
   } catch (error: any) {
-    // Catch any error that occurs and throw a new Error with the error message
+    throw new Error(error);
+  }
+};
+
+// Function to login a user with provided user data
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
     throw new Error(error);
   }
 };
