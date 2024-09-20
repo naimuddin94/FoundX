0d525fe9e4b4d6baf880311241e62553fa57aea7
naimuddin94 <developernaimuddin@gmail.com>
2024-09-20 20:44:27 +0600

    user login hook

--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
-import { registerUser } from "../services/AuthService";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
+import { registerUser } from "../services/AuthService";
 
 export const useUserRegistration = () => {
   return useMutation<any, Error, FieldValues>({
@@ -9,6 +9,19 @@ export const useUserRegistration = () => {
     mutationFn: async (userData) => await registerUser(userData),
     onSuccess: () => {
       toast.success("User registration successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export const useUserLogin = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_LOGIN"],
+    mutationFn: async (userData) => await registerUser(userData),
+    onSuccess: () => {
+      toast.success("Login successfully.");
     },
     onError: (error) => {
       toast.error(error.message);
