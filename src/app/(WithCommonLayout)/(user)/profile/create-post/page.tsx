00cd0bd8e5ef62b918a677bf0fcb539e92e26b2e
naimuddin94 <developernaimuddin@gmail.com>
2024-09-24 19:21:16 +0600

    build: update page.tsx update package.json

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -3,7 +3,7 @@
 import { allDistict } from "@bangladeshi/bangladesh-address";
 import { Button } from "@nextui-org/button";
 import { Divider } from "@nextui-org/divider";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import {
   FieldValues,
   FormProvider,
@@ -17,9 +17,10 @@ import FXDatePicker from "@/src/components/form/FXDatePicker";
 import FXInput from "@/src/components/form/FXInput";
 import FXSelect from "@/src/components/form/FXSelect";
 import { useUser } from "@/src/context/user.provider";
-import { dateToISOString } from "@/src/utils/dateToISO";
-import { useRouter } from "next/navigation";
 import { useGetCategories } from "@/src/hooks/categoreis.hook";
+import dateToISO from "@/src/utils/dateToISO";
+import { useRouter } from "next/navigation";
+import FXTextarea from "@/src/components/form/FXTextArea";
 
 const cityOptions = allDistict()
   .sort()
@@ -31,6 +32,18 @@ const cityOptions = allDistict()
   });
 
 export default function CreatePost() {
+  const [imageFiles, setImageFiles] = useState<File[] | []>([]);
+  const [imagePreviews, setImagePreviews] = useState<string[] | []>([]);
+
+  const router = useRouter();
+
+  // const {
+  //   mutate: handleCreatePost,
+  //   isPending: createPostPending,
+  //   isSuccess,
+  // } = useCreatePost();
+
+  const { user } = useUser();
 
   const {
     data: categoriesData,
@@ -38,9 +51,16 @@ export default function CreatePost() {
     isSuccess: categorySuccess,
   } = useGetCategories();
 
-  console.log(categoriesData);
+  let categoryOption: { key: string; label: string }[] = [];
 
-  const { user } = useUser();
+  if (categoriesData?.data && !categoryLoading) {
+    categoryOption = categoriesData.data
+      .sort()
+      .map((category: { _id: string; name: string }) => ({
+        key: category._id,
+        label: category.name,
+      }));
+  }
 
   const methods = useForm();
 
@@ -52,22 +72,53 @@ export default function CreatePost() {
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const formData = new FormData();
+
     const postData = {
       ...data,
       questions: data.questions.map((que: { value: string }) => que.value),
-      dateFound: dateToISOString(data.dateFound),
+      dateFound: dateToISO(data.dateFound),
       user: user!._id,
     };
 
     console.log(postData);
+
+    formData.append("data", JSON.stringify(postData));
+
+    for (let image of imageFiles) {
+      formData.append("itemImages", image);
+    }
+
+    // handleCreatePost(formData);
   };
 
   const handleFieldAppend = () => {
     append({ name: "questions" });
   };
 
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0];
+
+    setImageFiles((prev) => [...prev, file]);
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setImagePreviews((prev) => [...prev, reader.result as string]);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // if (!createPostPending && isSuccess) {
+  //   router.push("/");
+  // }
+
   return (
     <>
+      {/* {createPostPending && <Loading />} */}
       <div className="h-full rounded-xl bg-gradient-to-b from-default-100 px-[73px] py-12">
         <h1 className="text-2xl font-semibold">Post a found item</h1>
         <Divider className="mb-5 mt-3" />
@@ -87,6 +138,54 @@ export default function CreatePost() {
               </div>
               <div className="min-w-fit flex-1">
                 <FXSelect label="City" name="city" options={cityOptions} />
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXSelect
+                  disabled={!categorySuccess}
+                  label="Category"
+                  name="category"
+                  options={categoryOption}
+                />
+              </div>
+              <div className="min-w-fit flex-1">
+                <label
+                  className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
+                  htmlFor="image"
+                >
+                  Upload image
+                </label>
+                <input
+                  multiple
+                  className="hidden"
+                  id="image"
+                  type="file"
+                  onChange={(e) => handleImageChange(e)}
+                />
+              </div>
+            </div>
+
+            {imagePreviews.length > 0 && (
+              <div className="flex gap-5 my-5 flex-wrap">
+                {imagePreviews.map((imageDataUrl) => (
+                  <div
+                    key={imageDataUrl}
+                    className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
+                  >
+                    <img
+                      alt="item"
+                      className="h-full w-full object-cover object-center rounded-md"
+                      src={imageDataUrl}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+
+            <div className="flex flex-wrap-reverse gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXTextarea label="Description" name="description" />
               </div>
             </div>
 
